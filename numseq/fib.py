b5cb324521957e94af5b84ca3de380563fd9f96f
Kevin Clark <kevinwilliamclark@gmail.com>
2020-09-21 12:11:56 -0500

    adds fib function and numseq dir

---
+++ numseq/fib.py
@@ -0,0 +1,13 @@
+def fib(n):
+    a = 0
+    b = 1
+    if n <= 0:
+        print("Incorrect input")
+    elif n == 1:
+        return b
+    else:
+        for i in range(2, n):
+            c = a + b
+            a = b
+            b = c
+        return b
